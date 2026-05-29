2c4ca188caa507bf4cd9db1346b05ca3e31751bc
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-05-28 23:14:58 -0500

    Pizza tests passed

--- src/test/java/com/fazbears/model/PizzaTest.java
+++ src/test/java/com/fazbears/model/PizzaTest.java
@@ -15,7 +15,38 @@ class PizzaTest {
         assertEquals(12.00, actual, 0.01);
     }
 
+    @Test
+    void CalculatePriceShouldIncludeStuffedCrust() {
+        Pizza pizza = new Pizza("Test Pizza", 12, "Thin", true);
 
+        double actual = pizza.calculatePrice();
+
+        assertEquals(15.00, actual, 0.01);
+    }
+
+    @Test
+    void addToppingShouldPreventDuplicateToppings() {
+        Pizza pizza = new Pizza("Test Pizza", 12, "Thin", false);
+
+        boolean first = pizza.addTopping(new Meat("Pepperoni"));
+        boolean second = pizza.addTopping(new Meat("Pepperoni"));
+
+        assertTrue(first);
+        assertFalse(second);
+
+        assertEquals(1, pizza.getToppings().size());
+    }
+
+    @Test
+    void addToppingShouldPreventDuplicateToppingsIgnoringCase() {
+        Pizza pizza = new Pizza("Test Pizza", 12, "Thin", false);
+
+        pizza.addTopping(new Meat("Pepperoni"));
+        boolean second = pizza.addTopping(new Meat("pepperoni"));
+
+        assertFalse(second);
+        assertEquals(1, pizza.getToppings().size());
+    }
 
 
 }
